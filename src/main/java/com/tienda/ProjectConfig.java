a4a2669eebd0f7245ddac7b3fdaabaca960baea7
Ignacio Vargas <ignacii31@gmail.com>
2024-12-17 14:52:09 -0600

    Finalizacion Semana 11 - 12 - 13

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -1,13 +1,22 @@
-
 package com.tienda;
 
 import java.util.Locale;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -16,12 +25,12 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
  *
  * @author Ignac
  */
-
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
+
     /* Los siguientes métodos son para incorporar el tema de internacionalización en el proyecto */
 
-    /* localeResolver se utiliza para crear una sesión de cambio de idioma */
+ /* localeResolver se utiliza para crear una sesión de cambio de idioma */
     @Bean
     public LocaleResolver localeResolver() {
         var slr = new SessionLocaleResolver();
@@ -48,11 +57,97 @@ public class ProjectConfig implements WebMvcConfigurer {
     //Bean para poder acceder a los Messages.properties en código Java...
     @Bean("messageSource")
     public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource= new ResourceBundleMessageSource();
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
         messageSource.setBasenames("messages");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
-    
-    
+
+    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        //Si alguien trata de llegar a la ruta es las Index muestra de esta vista.
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+    }
+
+    /*
+    * El siguiente método se utiliza para completar la clase no es 
+    * realmente funcional, la próxima semana se reemplaza con usuarios de BD 
+    * Significado de la anotación @Bean: un objeto de configuración que se crea cuando se levanta la aplicación
+    * Y se llama Users acá dentro de este método vean que ese método dice que retorna un objeto de tipo user 
+    * Details Service.
+     */
+//    @Bean
+//    public UserDetailsService users() {
+//        /* --Explicacion del profesor
+//        * Entonces yo creo un objeto de tipo user details en ese caso lo voy
+//        * a llamar admin y mediante la clase de user utiliza el método binder 
+//        * y empieza a definir unos atributos.
+//        * Por ejemplo, yo voy a decirle, Ah, voy a crear un usuario.
+//        * Construyó un usuario con el user name Juan y con el Password 123.
+//         */
+//        UserDetails admin = User.builder()
+//                .username("juan")
+//                .password("{noop}123")
+//                .roles("USER", "VENDEDOR", "ADMIN")
+//                .build();
+//        UserDetails sales = User.builder()
+//                .username("rebeca")
+//                .password("{noop}456")
+//                .roles("USER", "VENDEDOR")
+//                .build();
+//        UserDetails user = User.builder()
+//                .username("pedro")
+//                .password("{noop}789")
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(user, sales, admin);
+//    }
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        build.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
+                .requestMatchers("/", "/index", "/errores/**", "/error",
+                        "/carrito/**", "/reportes/**",
+                        "/registro/**", "/js/**", "/webjars/**", "/error", "/refrescarBoton")
+                .permitAll()
+                /*
+                 * Vean que este es el paréntesis, abre y cierra entonces dice 
+                 * todos estos machos de acá va a tener permiso 
+                 * quien tenga el rol de admin.
+                 */
+                .requestMatchers(
+                        "/producto/nuevo", "/producto/guardar",
+                        "/producto/modificar/**", "/producto/eliminar/**",
+                        "/categoria/nuevo", "/categoria/guardar",
+                        "/categoria/modificar/**", "/categoria/eliminar/**",
+                        "/usuario/nuevo", "/usuario/guardar",
+                        "/usuario/modificar/**", "/usuario/eliminar/**",
+                        "/reportes/**", "/pruebas/**"
+                ).hasRole("ADMIN")
+                .requestMatchers(
+                        "/producto/listado",
+                        "/categoria/listado",
+                        "/usuario/listado"
+                ).hasRole("VENDEDOR")
+                .requestMatchers("/facturar/carrito")
+                .hasRole("USER")
+                )
+                .formLogin((form) -> form
+                .loginPage("/login").permitAll())
+                .logout((logout) -> logout.permitAll());
+        
+        return http.build();
+    }
+
 }
